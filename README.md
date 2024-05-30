46b5300838df1ec7555f516a7f2b708a9a6b5572
Morgan Delaney <hi@morgandelaney.me>
2024-05-30 08:39:51 -0700

    Adapt Chrome extension to Shortcut

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-# Pivotal Buttons Extension
-Chrome Extension for Adding Useful Pivotal Buttons
+# Shortcut Buttons Extension
+Chrome Extension for Adding Useful Shortcut Buttons
 
-![Buttons](./images/Pivotal-Buttons.png)
+![Buttons](./images/Shortcut-Buttons-Example.png)
 
 ## Features
-* Adds a button to copy the story Title to the clipboard
 * Adds a button to copy the story link in Markdown format
-* Modifies the existing "Copy Story ID" button to copy the story ID without the leading "#"
+
+## Todo
+* Add a button to copy the story Title to the clipboard
 
 ### How to load extension
 [Loading an unpacked extension](https://developer.chrome.com/docs/extensions/mv3/getstarted/development-basics/#load-unpacked)
@@ -14,5 +15,3 @@ Chrome Extension for Adding Useful Pivotal Buttons
 
 Guides and Inspiration
  * [Chrome Extension Tutorial](https://developer.chrome.com/docs/extensions/mv3/getstarted/)
- * [pivotal_markdown_link](https://github.com/hjdarnel/pivotal_markdown_link)
- * [easy-copy-for-pivotal-tracker](https://github.com/fukayatsu/easy-copy-for-pivotal-tracker)
--- scripts/content.js
+++ scripts/content.js
@@ -1,62 +1,43 @@
-/*
-    Construct a Markdown formatted link for text and a link
- */
-function makeMdLink(text, url) {
-    return '[' + text + '](' + url + ')';
-}
-
-/*
-    Remove existing added buttons, such as when maximizing one story then another
- */
-function removeButtons(container) {
-    [].slice.call(container.querySelectorAll('.pbe-title-link, .pbe-markdown-link')).forEach(element => {
-        element.remove();
-    });
-}
+let markdownTimer;
 
 /*
     Add the new buttons into the page.
  */
-function addButtons(copyLinkButton) {
-    const story = copyLinkButton.closest("form.story.model")
-    const storyName = story.querySelector("fieldset.name [name='story[name]']").value.replace(/'/g, '’');
-    const storyUrl = copyLinkButton.getAttribute('data-clipboard-text');
-    const storyMdLink = makeMdLink(storyName, storyUrl);
-
-    // Add button to copy the Markdown link to the clipboard
-    const mdLinkButton = document.createElement('button');
-    mdLinkButton.classList.add('autosaves', 'clipboard_button', 'link', 'left_endcap', 'hoverable', 'story-link', 'pbe-title-link');
-    mdLinkButton.setAttribute('type', 'button');
-    mdLinkButton.setAttribute('data-clipboard-text', storyMdLink);
-    mdLinkButton.setAttribute('title', 'Copy a Markdown link to this story');
-    mdLinkButton.setAttribute('tabIndex', '-1');
-    copyLinkButton.parentNode.insertBefore(mdLinkButton, copyLinkButton);
-
-    // Add button to copy the story Title to the clipboard
-    const titleButton = document.createElement('button');
-    titleButton.classList.add('autosaves', 'clipboard_button', 'link', 'left_endcap', 'hoverable', 'story-title', 'pbe-markdown-link');
-    titleButton.setAttribute('type', 'button');
-    titleButton.setAttribute('data-clipboard-text', storyName);
-    titleButton.setAttribute('title', 'Copy the story title');
-    titleButton.setAttribute('tabIndex', '-1');
-    copyLinkButton.parentNode.insertBefore(titleButton, copyLinkButton);
-
-    // The existing button to copy the story ID includes a #, which is not
-    // needed in most of my use cases. This removes it.
-    const idButton = document.querySelector('button.id');
-    const storyId = idButton.getAttribute('data-clipboard-text');
-    idButton.setAttribute('data-clipboard-text', storyId.replace(/#/g, ''));
-}
-
 function mutationCallback(mutationsList, observer) {
-    const selector = '.clipboard_button';
     mutationsList.forEach(mutation => {
-        if (mutation.target.matches('.story, .main.maximized') && mutation.target.querySelector(selector)) {
-            removeButtons(mutation.target);
-            addButtons(mutation.target.querySelector(selector));
+        // Listen for an open story
+        if (mutation.target.matches('.right-column')) {
+            const storyId = mutation.target.querySelector('.attribute.story-id');
+            const markdownButton = storyId?.querySelector('.markdown-button');
+            if (storyId && !markdownButton) {
+                // Gather data
+                const text = storyId.closest('.story-details').querySelector('.story-name').innerText.trim();
+                const url = storyId.nextElementSibling.querySelector('input').value;
+                // Add button to copy the Markdown link to the clipboard
+                const markdownButton = document.createElement('span');
+                storyId.appendChild(markdownButton);
+                markdownButton.className = 'markdown-button';
+                markdownButton.addEventListener('mousedown', async function (event) {
+                    event.stopPropagation();
+                    event.preventDefault();
+                    await copyToClipboard('[' + text + '](' + url + ')');
+                    // Display success icon
+                    markdownButton.className = 'markdown-button markdown-success';
+                    clearTimeout(markdownTimer);
+                    markdownTimer = setTimeout(() => markdownButton.className = 'markdown-button', 1000);
+                });
+            }
         }
     });
-    // we never call observer.disconnect() in case we open several stories throughout the session
+    // We never call observer.disconnect() in case we open several stories throughout the session
+}
+
+async function copyToClipboard(text) {
+    try {
+        await navigator.clipboard.writeText(text);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 const observer = new MutationObserver(mutationCallback);
